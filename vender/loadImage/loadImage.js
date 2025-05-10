eca4f920d0f26d9a9d2c67e4ebf343ac3a2bd586
ruibin.chow <ruibin.chow@qq.com>
2025-05-10 19:15:33 +0800

    fix load image script.

--- vender/loadImage/loadImage.js
+++ vender/loadImage/loadImage.js
@@ -10,7 +10,7 @@ function loadingImage() {
   const io = new IntersectionObserver((entries) =>{
     entries.forEach(item => {
       // isIntersecting是一个Boolean值，判断目标元素当前是否可见
-      if (item.isIntersecting) {
+      if (item.isIntersecting && !item.target.src.startsWith('data:image/')) {
         item.target.src = item.target.dataset.src;
         // 图片加载后即停止监听该元素
         io.unobserve(item.target);
